8a5305dea0c0505206694a5911563e37b0cee81e
xiaosage_chen <xiaosage_chen@hotmail.com>
2019-09-06 20:22:04 +0200

    a-module5-solution

---
+++ a-module5-solution/src/common/menu.service.js
@@ -0,0 +1,54 @@
+(function () {
+"use strict";
+
+angular.module('common')
+.service('MenuService', MenuService);
+
+
+MenuService.$inject = ['$http', 'ApiPath'];
+function MenuService($http, ApiPath) {
+  var service = this;
+  service.user = {};
+
+  service.saveUser = function (user) {
+    service.user = user;
+    console.log('user saved');
+    console.log('the user:', service.user);
+  }
+
+  service.getUser = function () {
+    return service.user;
+  }
+
+  service.getCategories = function () {
+    return $http.get(ApiPath + '/categories.json').then(function (response) {
+      return response.data;
+    });
+  };
+
+
+  service.getMenuItems = function (category) {
+    var config = {};
+    if (category) {
+      config.params = {'category': category};
+    }
+
+    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+      return response.data;
+    });
+  };
+
+
+  service.getFavoriteDish = function (shortname) {
+
+    return $http.get(ApiPath + '/menu_items/' + shortname + '.json');
+  };
+
+
+
+
+
+
+}
+
+})();
